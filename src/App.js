06d679e59d6bc028bc6473ed60120e1e6e1b4458
anton <avainfoconseils@gmail.com>
2024-07-31 08:48:33 +0200

    feat: add template code in text area

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 function App() {
+	const text = "function solution() {\n" +
+		"    \n" +
+		"}"
 	return (
 		<div className="app">
 			<div className="rules">
-				
+
 			</div>
 			<div className="editor">
-				<textarea className="textArea" spellCheck="false"></textarea>
+				<textarea className="textArea" spellCheck="false">
+					{text}
+				</textarea>
 			</div>
 		</div>
 	);
